19c5d024ff3626a5806c5461f4cad179dd427eb5
Relevantfender <ilic.miroslav1993@gmail.com>
2025-07-23 17:40:13 +0200

    test

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,77 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	mediaType, _, err = mime.ParseMediaType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error with the Content-Type header", err)
+		return
+	}
+	fileExtension, err := getFileExtension(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "No image extension found in the image", err)
+		return
+	}
+
+	var videoName string
+	rand.Read([]byte(videoName))
+	filePath := filepath.Join(
+		cfg.assetsRoot, fmt.Sprintf("%s.%s", videoName, fileExtension),
+	)
+	// filePath = "./" + filePath
+	newFile, err := os.Create(filePath)
+
+	_, err = io.Copy(newFile, file)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read the file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error while getting the video from db", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
+
+	video.ThumbnailURL = &filePath
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error while updating the video metadata", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
+}
+
+func getFileExtension(mediaType string) (string, error) {
+	if mediaType != "" {
+		data := strings.Split(strings.Trim(mediaType, " "), "/")
+		if len(data) == 2 {
+			return data[1], nil
+		}
+		return "", errors.New("No extension available in the header")
+	}
+
+	return "", errors.New("passed mediaType is an empty string")
+
 }
